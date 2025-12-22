c9ab0b09e4f257cc57e4c8ee9bd0d1884817af0f
Faiza Kashish <104677994+weebin-around@users.noreply.github.com>
2025-12-22 13:16:29 +0530

    Create erp_loc_info.sql

---
+++ scripts/silver/erp_loc_info.sql
@@ -0,0 +1,13 @@
+INSERT INTO silver.erp_loc_a101 (
+			cid,
+			cntry
+		)
+		SELECT
+			REPLACE(cid, '-', '') AS cid, 
+			CASE
+				WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+				ELSE TRIM(cntry)
+			END AS cntry -- Normalize and Handle missing or blank country codes
+		FROM bronze.erp_loc_a101;
