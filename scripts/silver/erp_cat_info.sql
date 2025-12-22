c762819b78d9ce175b8414691e779889bef2334d
Faiza Kashish <104677994+weebin-around@users.noreply.github.com>
2025-12-22 13:19:18 +0530

    Create erp_cat_info.sql

---
+++ scripts/silver/erp_cat_info.sql
@@ -0,0 +1,14 @@
+-- inserting data in the silver layer of the pipeline -- 
+
+INSERT INTO silver.erp_px_cat_g1v2 (
+			id,
+			cat,
+			subcat,
+			maintenance
+		)
+		SELECT
+			id,
+			cat,
+			subcat,
+			maintenance
+		FROM bronze.erp_px_cat_g1v2;
